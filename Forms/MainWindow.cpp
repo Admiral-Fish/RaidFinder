aace6cba496c040650d3e13f3384479a832760c9
Dominik Beese <44297391+Ich73@users.noreply.github.com>
2020-02-05 17:07:00 +0100

    remove help button on event data download dialog

--- Forms/MainWindow.cpp
+++ Forms/MainWindow.cpp
@@ -373,7 +373,7 @@ void MainWindow::downloadEventData()
         }
 
         bool flag;
-        QString item = QInputDialog::getItem(this, tr("Download event data"), tr("Event"), entries, 0, false, &flag);
+        QString item = QInputDialog::getItem(this, tr("Download event data"), tr("Event"), entries, 0, false, &flag, Qt::WindowTitleHint | Qt::WindowCloseButtonHint);
         if (!flag)
         {
             return;
