eeb69b2044cb577fbcaae9b320e0686c35d0e477
Admiral-Fish <admiralfish420@gmail.com>
2021-11-12 23:31:49 -0700

    Bump to Qt6

--- .ci/after_build.sh
+++ .ci/after_build.sh
@@ -3,12 +3,14 @@
 case $OS in
   linux)
   {
+    mv build/RaidFinder .
     zip -r RaidFinder-linux.zip RaidFinder
     sha256sum RaidFinder-linux.zip > RaidFinder-linux.zip.sha256
   } ;;  
   macOS)
   {
-    macdeployqt RaidFinder.app -dmg -verbose=2
+    mv build/RaidFinder.app .
+    PATH=$PATH:$HOME/Qt/6.1/macos/bin macdeployqt PokeFinder.app -dmg -verbose=2
     zip -r RaidFinder-macOS.zip RaidFinder.dmg
     shasum -a 256 RaidFinder-macOS.zip > RaidFinder-macOS.zip.sha256
   } ;;
